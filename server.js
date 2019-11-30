84bc919cb8bc16e9d7f949322f4fb57c37532719
Paul Shelton <paul.shelton@gmail.com>
2019-11-30 08:25:29 -0600

    initial commit

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ const forceSSL = function() {
 // middleware
 console.log("dir name " + __dirname);
 app.use(forceSSL());
-app.use(express.static(__dirname + '/dist'));
+
+app.use(express.static(__dirname + '/dist/map-app'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/map-app/index.html'));
   });
 
 app.listen(process.env.PORT || 8080);
